cb26cdb907c03d93ed62b121bc91b69031de71db
Jaystka <97751947+Jaystka@users.noreply.github.com>
2023-07-10 15:13:29 +0700

    Title

--- resources/views/retur/index.blade.php
+++ resources/views/retur/index.blade.php
@@ -1,7 +1,7 @@
 @extends('../layout/' . $layout)
 
 @section('subhead')
-<title>CRUD Data List - Icewall - Tailwind HTML Admin Template</title>
+<title>TUSTEL - Pengembalian</title>
 @endsection
 
 @section('subcontent')
